f11d6d6d6896ee9f37f645f376d30d99c4c80bf4
buymeagoat <53793097+buymeagoat@users.noreply.github.com>
2025-06-29 10:43:53 -0500

    Make DB connection attempts configurable

--- api/settings.py
+++ api/settings.py
@@ -16,6 +16,7 @@ class Settings(BaseSettings):
     log_to_stdout: bool = Field(False, env="LOG_TO_STDOUT")
     log_max_bytes: int = Field(10_000_000, env="LOG_MAX_BYTES")
     log_backup_count: int = Field(3, env="LOG_BACKUP_COUNT")
+    db_connect_attempts: int = Field(10, env="DB_CONNECT_ATTEMPTS")
     allow_registration: bool = Field(True, env="ALLOW_REGISTRATION")
     auth_username: str = Field("admin", env="AUTH_USERNAME")
     auth_password: str = Field("admin", env="AUTH_PASSWORD")
